341ad623cf25722b14657273745b470347feaf1d
Brian Iwana <biwana@gmail.com>
2018-11-02 16:02:33 +0900

    renaming in make-ucr

--- make-ucr.sh
+++ make-ucr.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-./run.sh generate_dataset_flexible_ucr_no_remove.py $1 random independent $2 && \
-./run.sh generate_dataset_flexible_ucr_no_remove.py $1 centers independent $2 && \
-./run.sh generate_dataset_flexible_ucr_no_remove.py $1 kcenters independent $2 && \
-./run.sh generate_dataset_flexible_ucr_no_remove.py $1 borders independent $2 && \
-./run.sh generate_dataset_flexible_ucr_no_remove.py $1 spanning independent $2
+./run.sh generate_dataset_flexible_ucr.py $1 random independent $2 && \
+./run.sh generate_dataset_flexible_ucr.py $1 centers independent $2 && \
+./run.sh generate_dataset_flexible_ucr.py $1 kcenters independent $2 && \
+./run.sh generate_dataset_flexible_ucr.py $1 borders independent $2 && \
+./run.sh generate_dataset_flexible_ucr.py $1 spanning independent $2
